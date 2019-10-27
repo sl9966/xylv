555c1f722d58f06cc5ca56b4c2700d51e041e8fe
ShengLiang <729678028@qq.com>
2019-10-27 17:18:01 +0800

    项目的搭建

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    <div class="header">头部</div>
+    <nuxt/>
+    <div class="footer">底部</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
